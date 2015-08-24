9455c8700629c81ad04862f9004d5d28a5a5a9f5
nicolethai <ThaiNicole@gmail.com>
2015-08-24 12:29:30 -0700

    New Start: mouse drawn.

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,17 +1,32 @@
-int WIDTH = 500;
-int HEIGHT = 500;
+int SCREEN_WIDTH = 500;
+int SCREEN_HEIGHT = 500;
+
+int mouseX = SCREEN_WIDTH/2;
+int mouseY = SCREEN_HEIGHT/2;
 
 void setup()
 {
-	size(WIDTH, HEIGHT);
+	size(SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 void draw()
 {
-	doSomething();
+	background(150);
+	mouse();
 }
 
-void doSomething()
+void mouse()
 {
+	noStroke();
 	fill(255, 255, 150, 100);
-	ellipse (100, 100, 25, 25);
+	ellipse (mouseX, mouseY, 45, 30);
+	noFill();
+	stroke(0);
+	bezier(mouseX + 15, mouseY, mouseX+50, mouseY+5, mouseX+55, mouseY-5, mouseX+60, mouseY);
+	// nose/mouth area
+	// fill(0);
+	// beginShape();
+	// 	vertex(mouseX-20, mouseY+5);
+	// 	vertex(mouseX-40, mouseY+15);
+	// 	vertex(mouseX-20, mouseY+20);
+	// endShape(CLOSE);
 }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,22 +14,37 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-int WIDTH = 500;
-int HEIGHT = 500;
+int SCREEN_WIDTH = 500;
+int SCREEN_HEIGHT = 500;
+
+int mouseX = SCREEN_WIDTH/2;
+int mouseY = SCREEN_HEIGHT/2;
 
 public void setup()
 {
-	size(WIDTH, HEIGHT);
+	size(SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 public void draw()
 {
-	doSomething();
+	background(150);
+	mouse();
 }
 
-public void doSomething()
+public void mouse()
 {
+	noStroke();
 	fill(255, 255, 150, 100);
-	ellipse (100, 100, 25, 25);
+	ellipse (mouseX, mouseY, 45, 30);
+	noFill();
+	stroke(0);
+	bezier(mouseX + 15, mouseY, mouseX+50, mouseY+5, mouseX+55, mouseY-5, mouseX+60, mouseY);
+	// nose/mouth area
+	// fill(0);
+	// beginShape();
+	// 	vertex(mouseX-20, mouseY+5);
+	// 	vertex(mouseX-40, mouseY+15);
+	// 	vertex(mouseX-20, mouseY+20);
+	// endShape(CLOSE);
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
